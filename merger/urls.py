560da9a78af79218907de110d709ada112b3c073
JustTrying652 <ngataevans@gmail.com>
2026-08-13 12:52:16 +0300

    merger redirect url

---
+++ merger/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("auth/login/", views.login, name="spotify-login"),
+    path("auth/callback/", views.callback, name="spotify-callback"),
+    path("playlists/", views.my_playlists, name="my-playlists"),
+    path("merge/", views.merge_playlists, name="merge-playlists"),
+]
