fc6e7ca34f9e6b91143521b420efa86e862bc22d
JustTrying652 <ngataevans@gmail.com>
2026-08-25 10:23:56 +0300

    update imports

--- merger/views.py
+++ merger/views.py
@@ -7,7 +7,7 @@ from rest_framework.response import Response
 
 from .models import MergeHistory
 from .services.spotipy_official import OfficialSpotipyClient
-from .services.dedup import extract_tracks, merge_unique, find_overlap, find_near_duplicates, format_duration
+from .services.dedup import extract_tracks, merge_unique, find_overlap, find_near_duplicates, format_duration, apply_selections
 
 client = OfficialSpotipyClient()
 
